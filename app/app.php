956cb30e3620fbb3326aace1cf8c2251e00d3d34
rebecca allen <rebeccazarsky@gmail.com>
2016-09-29 15:46:40 -0400

    add some of the rough routing

--- app/app.php
+++ app/app.php
@@ -14,6 +14,9 @@
 
   $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
+  use Symfony\Component\HttpFoundation\Request;
+  Request::enableHttpMethodParameterOverride();
+
   $app->get("/", function() use ($app)
     {
       return $app['twig']->render('home.html.twig');
@@ -29,10 +32,33 @@
       return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
     });
 
-  // $app->post("/added_client", function() use ($app)
-  // {
-  //   return
-  // });
+  $app->post("/added_client", function() use ($app)
+  {
+    $new_client = new Client($_POST(['name'], ['stylist_id']));
+    $new_client->save();
+    return $app['twig']->render('clients.html.twig', array('clients' =>Client::getAll()));
+  });
+
+  $app->post("/added_stylist", function() use ($app)
+  {
+    $new_stylist = new Stylist($_POST['name']);
+    $new_stylist->save();
+    return $app['twig']->render('stylists.html.twig', array('stylists' =>Stylist::getAll()));
+  });
+
+  $app->patch("/edited_client/{id}", function() use ($app)
+  {
+    $client = Client::find($id);
+    $client->updateName($_POST['new_name']);
+    return $app['twig']->render('clients.html.twig', array('clients' => Client::getAll()));
+  });
+
+  $app->patch("/edited_stylist/{id}", function() use ($app)
+  {
+    $stylist = Stylist::find($id);
+    $stylist->updateName($_POST['new_name']);
+    return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
+  });
 
   return $app;
 ?>
